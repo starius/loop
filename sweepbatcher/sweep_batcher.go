1e3e70f289174c53a4af73b6d9173c4aa66cc0d0
Boris Nagaev <bnagaev@gmail.com>
2025-04-08 12:34:42 -0300

    sweepbatcher: unblock sending if notifier quits

--- sweepbatcher/sweep_batcher.go
+++ sweepbatcher/sweep_batcher.go
@@ -1100,7 +1100,15 @@ func (b *Batcher) monitorSpendAndNotify(ctx context.Context, sweep *sweep,
 				}
 
 				select {
+				// Try to write the update to the notification
+				// channel.
 				case notifier.SpendChan <- spendDetail:
+
+				// If a quit signal was provided by the swap,
+				// continue.
+				case <-notifier.QuitChan:
+
+				// If the context was canceled, stop.
 				case <-ctx.Done():
 				}
 
@@ -1108,7 +1116,15 @@ func (b *Batcher) monitorSpendAndNotify(ctx context.Context, sweep *sweep,
 
 			case err := <-spendErr:
 				select {
+				// Try to write the error to the notification
+				// channel.
 				case notifier.SpendErrChan <- err:
+
+				// If a quit signal was provided by the swap,
+				// continue.
+				case <-notifier.QuitChan:
+
+				// If the context was canceled, stop.
 				case <-ctx.Done():
 				}
 
@@ -1118,9 +1134,11 @@ func (b *Batcher) monitorSpendAndNotify(ctx context.Context, sweep *sweep,
 
 				return
 
+			// If a quit signal was provided by the swap, continue.
 			case <-notifier.QuitChan:
 				return
 
+			// If the context was canceled, stop.
 			case <-ctx.Done():
 				return
 			}
