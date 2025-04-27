a67a666737cd115770983674cb20438ccb1b0544
Boris Nagaev <bnagaev@gmail.com>
2025-04-27 01:08:41 -0300

    sweepbatcher: fix OnChainFeePortion values
    There were two mistakes.
    
    In case of a swap with multiple sweeps only the fee of the first sweep
    of a swap was accounted.
    
    Rounding diff (the remainder) was attributed to all the sweeps rather than to
    the first (primary) sweep of the batch. The sweep to attribute the remainder
    was chosen by comparing SignatureScript which is always empty. New approach is
    to find the primary sweep and to compare its outpoint directly.

--- sweepbatcher/sweep_batcher.go
+++ sweepbatcher/sweep_batcher.go
@@ -778,7 +778,7 @@ func (b *Batcher) handleSweeps(ctx context.Context, sweeps []*sweep,
 		// and we won't be able to attach this sweep to it.
 		if parentBatch.Confirmed {
 			return b.monitorSpendAndNotify(
-				ctx, sweep, parentBatch.ID, notifier,
+				ctx, sweeps, parentBatch.ID, notifier,
 			)
 		}
 	}
@@ -1061,7 +1061,7 @@ func (b *Batcher) FetchUnconfirmedBatches(ctx context.Context) ([]*batch,
 // the response back to the response channel. It is called if the batch is fully
 // confirmed and we just need to deliver the data back to the caller though
 // SpendNotifier.
-func (b *Batcher) monitorSpendAndNotify(ctx context.Context, sweep *sweep,
+func (b *Batcher) monitorSpendAndNotify(ctx context.Context, sweeps []*sweep,
 	parentBatchID int32, notifier *SpendNotifier) error {
 
 	// If the caller has not provided a notifier, stop.
@@ -1078,6 +1078,17 @@ func (b *Batcher) monitorSpendAndNotify(ctx context.Context, sweep *sweep,
 
 		return err
 	}
+
+	// Find the primarySweepID.
+	dbSweeps, err := b.store.FetchBatchSweeps(ctx, parentBatchID)
+	if err != nil {
+		cancel()
+
+		return err
+	}
+	primarySweepID := dbSweeps[0].Outpoint
+
+	sweep := sweeps[0]
 
 	spendChan, spendErr, err := b.chainNotifier.RegisterSpendNtfn(
 		spendCtx, &sweep.outpoint, sweep.htlc.PkScript,
@@ -1099,6 +1110,7 @@ func (b *Batcher) monitorSpendAndNotify(ctx context.Context, sweep *sweep,
 		select {
 		case spend := <-spendChan:
 			spendTx := spend.SpendingTx
+
 			// Calculate the fee portion that each sweep should pay
 			// for the batch.
 			feePortionPerSweep, roundingDifference :=
@@ -1107,17 +1119,23 @@ func (b *Batcher) monitorSpendAndNotify(ctx context.Context, sweep *sweep,
 					totalSwept,
 				)
 
-			onChainFeePortion := getFeePortionPaidBySweep(
-				spendTx, feePortionPerSweep,
-				roundingDifference, sweep,
-			)
+			// Add onchain fee accross all the sweeps of the swap.
+			var fee btcutil.Amount
+			for _, s := range sweeps {
+				isFirst := s.outpoint == primarySweepID
+
+				fee += getFeePortionPaidBySweep(
+					spendTx, feePortionPerSweep,
+					roundingDifference, isFirst,
+				)
+			}
 
 			// Notify the requester of the spend with the spend
 			// details, including the fee portion for this
 			// particular sweep.
 			spendDetail := &SpendDetail{
 				Tx:                spendTx,
-				OnChainFeePortion: onChainFeePortion,
+				OnChainFeePortion: fee,
 			}
 
 			select {
