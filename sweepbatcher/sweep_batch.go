59da056e89e956e64bab7a14f9b8342f6dfe4f97
Boris Nagaev <bnagaev@gmail.com>
2025-06-24 13:06:58 -0300

    sweepbatcher: check of conf notification has tx

--- sweepbatcher/sweep_batch.go
+++ sweepbatcher/sweep_batch.go
@@ -2060,6 +2060,10 @@ func (b *batch) handleConf(ctx context.Context,
 	conf *chainntnfs.TxConfirmation) error {
 
 	spendTx := conf.Tx
+	if spendTx == nil {
+		return fmt.Errorf("confirmation doesn't have spendTx, "+
+			"height=%d, TxIndex=%d", conf.BlockHeight, conf.TxIndex)
+	}
 	txHash := spendTx.TxHash()
 	if b.batchTxid == nil || *b.batchTxid != txHash {
 		b.Warnf("Mismatch of batch txid: tx in spend notification had "+
